75be61f53dbf031c3028df6e89fad35da8702cb1
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2025-05-14 15:12:41 +0300

    feat: guessing game task resolved

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.min = null;
+    this.max = null;
+    this.guessValue = null;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-  lower() { }
+  guess() {
+    this.guessValue = Math.ceil((this.min + this.max) / 2);
+    return this.guessValue;
+  }
 
-  greater() { }
-*/
+  lower() {
+    this.max = this.guessValue;
+  }
+
+  greater() {
+    this.min = this.guessValue;
+  }
 }
 
 module.exports = GuessingGame;
